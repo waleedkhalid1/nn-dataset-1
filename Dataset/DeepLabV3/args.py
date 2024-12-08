31b167661318875e90a7c4eb2ab95aa024b3b778
SekkaSKS <alduinminami@gmail.com>
2024-12-08 22:33:42 +0100

    adjust segmentation model args

--- Dataset/DeepLabV3/args.py
+++ Dataset/DeepLabV3/args.py
@@ -4,8 +4,8 @@ from .code import Bottleneck, FCNHead, ResNet, DeepLabHead
 from torch import nn
 
 
-backbones: Dict[str, List[nn.Module]] = {
-    "ResNet50": [ResNet(Bottleneck, [3, 4, 6, 3],num_classes=100 , replace_stride_with_dilation=[False, True, True]),DeepLabHead(2048,100),None],
-    "ResNet101": [ResNet(Bottleneck, [3, 4, 23, 3], replace_stride_with_dilation=[False, True, True]),DeepLabHead(2048,100),None],
+backbones: Dict[str, dict] = {
+    "ResNet50": [ResNet(Bottleneck, [3, 4, 6, 3], num_classes=100, replace_stride_with_dilation=[False, True, True]),DeepLabHead(2048,21),None],
+    "ResNet101": [ResNet(Bottleneck, [3, 4, 23, 3], num_classes=100, replace_stride_with_dilation=[False, True, True]),DeepLabHead(2048,21),None],
 }
 args = [*backbones["ResNet50"]]
--- Dataset/LRASPP/args.py
+++ Dataset/LRASPP/args.py
@@ -1,3 +1,3 @@
 from .code import Net, MobileNetV3, _mobilenet_v3_conf
 
-args = [MobileNetV3(*_mobilenet_v3_conf("mobilenet_v3_large"), num_classes = 100), None, None, 100]
+args = [MobileNetV3(*_mobilenet_v3_conf("mobilenet_v3_large"), num_classes = 100), None, None, 21]
