79c57b4a73ff90a4aef4ebe8e5701cceaac68b62
SekkaSKS <alduinminami@gmail.com>
2024-12-08 15:58:24 +0100

    fcn8 fcn16 fcn32

---
+++ Dataset/FCN32s/args.py
@@ -0,0 +1,12 @@
+from typing import Dict, List
+
+from torch import nn
+
+from .code import ResNet, Bottleneck, FCNHead, VGG, make_layers, vgg_cfgs
+
+backbones: Dict[str, List[nn.Module]] = {
+    "ResNet50": [ResNet(Bottleneck, [3, 4, 6, 3], num_classes = 100, replace_stride_with_dilation=[False, True, True]),FCNHead(2048, 100)],
+    "ResNet101": [ResNet(Bottleneck, [3, 4, 23, 3], num_classes = 100, replace_stride_with_dilation=[False, True, True]),FCNHead(2048, 100)],
+    "VGG16": [VGG(make_layers(vgg_cfgs["D"]),num_classes=100),FCNHead(512, 100)],
+}
+args = [*backbones["ResNet50"]]
