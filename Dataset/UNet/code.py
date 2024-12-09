3b96bcb5990c28abd2b48cdc6eece268899ea139
SekkaSKS <alduinminami@gmail.com>
2024-12-09 23:19:46 +0100

    fix a mistake in unet

--- Dataset/UNet/code.py
+++ Dataset/UNet/code.py
@@ -3,16 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class DoubleConv(nn.Module):
-    def __init__(self, num_classes=100, mid_channels=None):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
         if not mid_channels:
-            mid_channels = num_classes
+            mid_channels = out_channels
         self.double_conv = nn.Sequential(
-            nn.Conv2d(3, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, num_classes, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(num_classes),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
