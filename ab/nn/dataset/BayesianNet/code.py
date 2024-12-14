cf001fc041ca06c23589cae8762eaa06f2b54c7a
ABrain-One <112828851+ABrain-One@users.noreply.github.com>
2024-12-14 14:07:02 +0100

    Update code.py

--- ab/nn/dataset/BayesianNet/code.py
+++ ab/nn/dataset/BayesianNet/code.py
@@ -186,6 +186,8 @@ class BBBConv2d(ModuleWrapper):
             kl += calculate_kl(self.prior_mu, self.prior_sigma, self.bias_mu, self.bias_sigma)
         return kl
 
+args = []
+
 class Net(ModuleWrapper):
     """
     Simple Neural Network having 3 Convolution
