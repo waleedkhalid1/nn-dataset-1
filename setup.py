0caff5c660c29ae09a3d1082e63a7b59f82adce0
ABrain-One <112828851+ABrain-One@users.noreply.github.com>
2024-12-19 12:36:06 +0100

    python_requires=">=3.10"

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.10",
     include_package_data=True,
 )
 
