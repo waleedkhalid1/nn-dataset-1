c70c6aa62b5b91d5cd000a194ccdea61d264a676
ABrain-One <AI-Linux@ABrain.one>
2024-12-19 17:33:43 +0100

    NN statistics

--- ab/nn/train.py
+++ ab/nn/train.py
@@ -213,10 +213,10 @@ if __name__ == "__main__":
     # conf = ['img_classification', 'img_segmentation']  # For all image classification and segmentation configurations
 
     # Number of Optuna trial examples.
-    # optuna_trials = 100 # 100 trials
+    optuna_trials = 100 # 100 trials
     # optuna_trials = "+1"  # Try once more. For quick verification of model training after code modifications.
-    optuna_trials = "+5"  # Try 5 more times. To thoroughly verify model training after code modifications.
+    # optuna_trials = "+5"  # Try 5 more times. To thoroughly verify model training after code modifications.
 
-    ''' Please commit updated statistics whenever you have new performance results '''
+    ''' !!! Commit updated statistics whenever new results are available !!! '''
     # Run training with Optuna: detects and saves performance metric values for a varying number of epochs
     main(conf, [1, 2, 5], optuna_trials)
