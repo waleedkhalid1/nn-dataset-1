ca7502b60cd0416ef91813a1cad5dd929e72eff9
ABrain-One <AI-Linux@ABrain.one>
2024-12-21 23:18:41 +0100

    Default args: refactoring

--- ab/nn/util/Const.py
+++ ab/nn/util/Const.py
@@ -5,6 +5,6 @@ nn_module = str.replace(nn_dir, os.sep, '.')
 default_config = ''
 default_epochs = (1, 2, 5)
 default_trials = 100
-default_from_root = False
+default_from_root = True
 default_batch_power = 6
 global from_root_g
