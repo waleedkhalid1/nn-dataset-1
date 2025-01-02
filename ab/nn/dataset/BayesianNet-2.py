fd2472738b1a491dfb254f93d583488951958c34
ABrain-One <AI-Linux@ABrain.one>
2025-01-02 16:42:09 +0100

    Support for a variable number of arbitrary hyperparameter names, each with a value range from 0.0 to 1.0.

--- ab/nn/dataset/BayesianNet-2.py
+++ ab/nn/dataset/BayesianNet-2.py
@@ -181,7 +181,9 @@ class BBBConv2d(ModuleWrapper):
 def supported_hyperparameters():
     return {'lr', 'momentum'}
 
+
 class Net(ModuleWrapper):
+
 
     def train_setup(self, device, prm):
         self.device = device
@@ -211,22 +213,26 @@ class Net(ModuleWrapper):
         }
         self.act = nn.Softplus
         
-        self.conv1 = BBBConv2d(inputs, 6, 5, padding=0, bias=True, priors=self.priors)
+        self.conv1 = BBBConv2d(inputs, 64, 11, stride=4, padding=5, bias=True, priors=self.priors)
         self.act1 = self.act()
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv2 = BBBConv2d(6, 16, 5, padding=0, bias=True, priors=self.priors)
+        self.conv2 = BBBConv2d(64, 192, 5, padding=2, bias=True, priors=self.priors)
         self.act2 = self.act()
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.flatten = FlattenLayer(5 * 5 * 16)
-        self.fc1 = BBBLinear(5 * 5 * 16, 120, bias=True, priors=self.priors)
+        self.conv3 = BBBConv2d(192, 384, 3, padding=1, bias=True, priors=self.priors)
         self.act3 = self.act()
 
-        self.fc2 = BBBLinear(120, 84, bias=True, priors=self.priors)
+        self.conv4 = BBBConv2d(384, 256, 3, padding=1, bias=True, priors=self.priors)
         self.act4 = self.act()
 
-        self.fc3 = BBBLinear(84, outputs, bias=True, priors=self.priors)
+        self.conv5 = BBBConv2d(256, 128, 3, padding=1, bias=True, priors=self.priors)
+        self.act5 = self.act()
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.flatten = FlattenLayer(1 * 1 * 128)
+        self.classifier = BBBLinear(1 * 1 * 128, outputs, bias=True, priors=self.priors)
 
     def __call__(self, *args):
         return super().__call__(*args)[0]
--- ab/nn/dataset/InceptionV3-1.py
+++ ab/nn/dataset/InceptionV3-1.py
@@ -53,7 +53,7 @@ class Net(nn.Module):
         inception_e = inception_blocks[5]
         inception_aux = inception_blocks[6]
 
-        self.aux_logits = True
+        self.aux_logits = False
         self.transform_input = transform_input
         self.Conv2d_1a_3x3 = conv_block(in_shape[1], 32, kernel_size=3, stride=2)
         self.Conv2d_2a_3x3 = conv_block(32, 32, kernel_size=3)
